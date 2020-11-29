580c7f26927e500375461f624f1da39300d095ae
Nsadow311 <titandestroyer0311@gmail.com>
2020-11-29 03:46:28 -0500

    Code go brrrr more

--- src/pages/LandingPage/index.jsx
+++ src/pages/LandingPage/index.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Box, Heading, Text, Button } from '@chakra-ui/core'
 import './landingpage.css';
 export function LandingPage(props) {
-    const login = () => window.location.href = 'http://localhost:3001/api/auth/discord';
+    const login = () => window.location.href = '/auth';
     return (
      <div className="landing-root">
          <Box className="main-box">
